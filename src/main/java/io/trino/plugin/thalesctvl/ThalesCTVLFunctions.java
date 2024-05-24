a13192d94aab1835603e82c913e4cbc0f4cf6799
markbwarner <markbwarner@gmail.com>
2024-05-24 15:02:44 -0400

    Update ThalesCTVLFunctions.java

--- src/main/java/io/trino/plugin/thalesctvl/ThalesCTVLFunctions.java
+++ src/main/java/io/trino/plugin/thalesctvl/ThalesCTVLFunctions.java
@@ -13,13 +13,6 @@
  */
 package io.trino.plugin.thalesctvl;
 
-import com.google.cloud.functions.HttpFunction;
-import com.google.cloud.functions.HttpRequest;
-import com.google.cloud.functions.HttpResponse;
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
 import io.airlift.slice.Slice;
 import io.airlift.slice.Slices;
 import io.trino.spi.function.Description;
@@ -27,21 +20,19 @@ import io.trino.spi.function.ScalarFunction;
 import io.trino.spi.function.SqlNullable;
 import io.trino.spi.function.SqlType;
 import io.trino.spi.type.StandardTypes;
-import java.io.*;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.security.Security;
-import java.util.Base64;
-import javax.crypto.Cipher;
+
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSession;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Base64;
 
 public final class ThalesCTVLFunctions
 {
@@ -54,70 +45,78 @@ public final class ThalesCTVLFunctions
     {
         //String userid =  System.getenv("CTSUSER");
         //String password =  System.getenv("CTSPWD");
-		//String ctsip = System.getenv("CTSIP");
+        String userid = "cts-user";
+        String password = "yourpwd";
+        String ctsip = "yourip";
+        String idpassword = userid + ":" + password;
+        String encodedidpwd = null;
+        disableCertValidation();
+        encodedidpwd = Base64.getEncoder().encodeToString(idpassword.getBytes("UTF-8"));
+        String httpsurl = "https://" + ctsip + "/vts/rest/v2.0/tokenize/";
+        URL myurl = new URL(httpsurl);
+        HttpsURLConnection con = (HttpsURLConnection) myurl.openConnection();
+        String jStr = "{\"data\":\"" + inputstring + "\",\"tokengroup\":\"tg1\",\"tokentemplate\":\"tt1\"}";
+        con.setRequestProperty("Content-length", String.valueOf(jStr.length()));
+        con.setRequestProperty("Content-Type", "application/json");
+        con.setRequestProperty("Authorization", "Basic " + encodedidpwd);
+        con.setRequestMethod("POST");
+        con.setDoOutput(true);
+        con.setDoInput(true);
+        DataOutputStream output = new DataOutputStream(con.getOutputStream());
+        output.writeBytes(jStr);
+        output.close();
+        BufferedReader rd = new BufferedReader(new InputStreamReader(con.getInputStream()));
 
-		String userid = "cts-user";
-		String password = "yourpwd";
-		String ctsip = "yourip";
-		String idpassword = userid + ":" + password;
-		String encodedidpwd = null;
-		disableCertValidation();
-			encodedidpwd = Base64.getEncoder().encodeToString(idpassword.getBytes("UTF-8"));
-	        String https_url = "https://" + ctsip + "/vts/rest/v2.0/tokenize/";
-		URL myurl = new URL(https_url);
-		HttpsURLConnection con = (HttpsURLConnection) myurl.openConnection();
-		String jStr = "{\"data\":\"" + inputstring + "\",\"tokengroup\":\"tg1\",\"tokentemplate\":\"tt1\"}";
-		con.setRequestProperty("Content-length", String.valueOf(jStr.length()));
-		con.setRequestProperty("Content-Type", "application/json");
-		con.setRequestProperty("Authorization", "Basic " + encodedidpwd);
-		con.setRequestMethod("POST");
-		con.setDoOutput(true);
-		con.setDoInput(true);
-		DataOutputStream output = new DataOutputStream(con.getOutputStream());
-		output.writeBytes(jStr);
-		output.close();
-		BufferedReader rd = new BufferedReader(new InputStreamReader(con.getInputStream()));
-
-		String line = "";
-		String strResponse = "";
-		while ((line = rd.readLine()) != null) {
-			strResponse = strResponse + line;
-		}
-		rd.close();
-		con.disconnect();
-		System.out.println("Tokenize server: " + https_url);
-		System.out.println("Tokenize request: " + jStr);
-		System.out.println("Tokenize response: " + strResponse);
+        String line = "";
+        String strResponse = "";
+        while ((line = rd.readLine()) != null) {
+            strResponse = strResponse + line;
+        }
+        rd.close();
+        con.disconnect();
+        System.out.println("Tokenize server: " + https_url);
+        System.out.println("Tokenize request: " + jStr);
+        System.out.println("Tokenize response: " + strResponse);
         return (Slices.utf8Slice(strResponse));
     }
-    
-  	public static void disableCertValidation() throws Exception 
-  	{
-		TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
-			public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-				return null;
-			}
-			public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType) {
-			}
-			public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType) {
-			}
-		} };
 
-		// Install the all-trusting trust manager
-		try {
-			SSLContext sc = SSLContext.getInstance("SSL");
-			sc.init(null, trustAllCerts, new java.security.SecureRandom());
-			HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		// Create all-trusting host name verifier
-		HostnameVerifier allHostsValid = new HostnameVerifier() {
-			public boolean verify(String hostname, SSLSession session) {
-				return true;
-			}
-		};
-		// Install the all-trusting host verifier
-		HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
-	}
+    public static void disableCertValidation()
+            throws Exception
+    {
+        TrustManager[] trustAllCerts = new TrustManager[]
+        {
+            new X509TrustManager()
+            {
+                public java.security.cert.X509Certificate[] getAcceptedIssuers()
+                {
+                    return null;
+                }
+
+                public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType)
+                {
+                }
+
+                public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType)
+                {
+                }
+                }
+        };
+        try {
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    // Create all-trusting host name verifier
+        HostnameVerifier allHostsValid = new HostnameVerifier()
+        {
+            public boolean verify(String hostname, SSLSession session)
+            {
+                return true;
+            }
+        };
+        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+    }
 }
